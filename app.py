93a17620d29bc4a36a9a52365915c703d51e09a1
Pranav-Pedaballe <pranav_rp1@ece.IItr.ac.in>
2025-06-25 09:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 
 # Load model
-model = load_model("final_model_resaved(1).keras")
+model = load_model("final_model_saved.keras")
 
 # Prepare label encoder (same order as training)
 emotions = ['angry', 'calm', 'disgust', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
