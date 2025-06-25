73c55ff36139b16844e6ad6522d3bafb1d14c877
Pranav-Pedaballe <pranav_rp1@ece.IItr.ac.in>
2025-06-25 18:26:10 +0530

    Update convert_model.py

--- convert_model.py
+++ convert_model.py
@@ -1,20 +1,15 @@
-# convert_model.py
-
 from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import InputLayer as _InputLayer
 
-# Patch InputLayer to ignore 'batch_shape' kwarg
 def InputLayer(*args, **kwargs):
     kwargs.pop("batch_shape", None)
     return _InputLayer(*args, **kwargs)
 
-# Load with our patched InputLayer
 model = load_model(
     "final_model.h5",
     compile=False,
     custom_objects={"InputLayer": InputLayer}
 )
 
-# Re-save in pure HDF5 (Keras 2.13 format)
-model.save("final_model_streamlit.h5")
-print("✅ Saved final_model_streamlit.h5")
+model.save("final_model_stream.h5")
+print("✅ Saved final_model_stream.h5")
